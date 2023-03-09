f4f9ee9350444e8807f64c314d4ea7f35e53ea87
Your Name <smile2sasi95@gmail.com>
2023-03-09 20:18:02 +0530

    add

---
+++ main.js
@@ -0,0 +1,45 @@
+//console.log('java script')
+
+
+
+var x=parseInt(prompt());
+function pattern (){
+	for (i=1;i<=x; i++){
+     for(j=1;j<=i; j++){
+      console.log(j);
+	 }
+	}
+}
+pattern()
+
+//data types
+var a="5", b=7,
+c=a+b
+console.log(c);
+
+var d=5; e=8;
+f=d+e
+console.log(typeof f);
+
+var g=5; h=6;
+i=g>h
+console.log(typeof i);
+
+var a=parseInt(prompt());
+ function add(){
+	 for (i=1;i<=a;i++){
+	 for(j=1;j<=i;j++){
+		 console.log(i);
+		 }
+	 }
+ }
+ add();
+
+function add(){
+	let a=10,b=12,c;
+	c=(a+b);
+	return c;
+}
+
+
+
